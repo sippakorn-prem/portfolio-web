2917279f65726bf0a7b118c73da30f669973809a
PREMio <sippakorn@conicle.com>
2023-05-04 02:39:25 +0700

    refactor styles filename

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: ['./src'],
-    prependData: `@use 'globals' as *;`,
+    prependData: `@use 'variables' as *;`,
   },
 }
 

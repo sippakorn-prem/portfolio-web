9d63de534bb4d4e08f28cb45314121aa7f920fdb
PREMio <sippakorn@conicle.com>
2023-05-07 00:36:08 +0700

    add animation in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,84 @@ import styles from './index.module.scss';
 import Layout from '@/components/Layout';
 import { ReactElement, useEffect } from 'react';
 import { Typography } from '@mui/material';
-import { grey } from '@mui/material/colors';
-import { setIntesectionObserver } from '@/utils';
+import anime from 'animejs';
 
 export default function Home(): ReactElement {
   useEffect(() => {
-    // setIntesectionObserver(styles.show, styles.hidden);
+    const wrapper = `.${styles.background} .${styles.paragraphWrapper}`;
+    const line = `${wrapper} .${styles.name} .${styles.line}`;
+    const name = `${wrapper} .${styles.name} span`;
+    const position = `${wrapper} .${styles.position} span`;
+    anime
+      .timeline({ delay: 1000 })
+      .add({
+        targets: line,
+        scaleY: [0, 1],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 700,
+      })
+      .add({
+        targets: line,
+        translateX: [
+          -10,
+          (document?.querySelector(`${wrapper} .${styles.name}`)?.getBoundingClientRect()?.width ??
+            0) + 10,
+        ],
+        easing: 'easeOutExpo',
+        duration: 700,
+        delay: 100,
+      })
+      .add(
+        {
+          targets: name,
+          opacity: [0, 1],
+          easing: 'easeOutExpo',
+          duration: 600,
+          delay: (el, i) => 20 * (i + 1),
+        },
+        '-=775'
+      )
+      .add(
+        {
+          targets: line,
+          scaleY: [1, 0],
+          opacity: [1, 0],
+          easing: 'easeOutExpo',
+          duration: 700,
+        },
+        '-=300'
+      )
+      .add(
+        {
+          targets: position,
+          translateX: [40, 0],
+          translateZ: 0,
+          opacity: [0, 1],
+          easing: 'easeOutExpo',
+          duration: 1200,
+          delay: (el, i) => 500 + 30 * i,
+        },
+        '-=450'
+      );
   }, []);
 
   return (
     <Layout>
       <div className={styles.background}>
-        <Typography className={styles.paragraph} variant="h1">
-          Sippakorn Suphapinyo
-        </Typography>
-        <Typography className={styles.paragraph} variant="h1">
-          Sippakorn Suphapinyo
-        </Typography>
-        <h1 className={styles.paragraph}>Sippakorn Suphapinyo</h1>
+        <div className={styles.paragraphWrapper}>
+          <Typography className={styles.name} variant="h2">
+            <div className={styles.line}>|</div>
+            {'Sippakorn Suphapinyo'.split('').map((letter, i) => (
+              <span key={i}>{letter}</span>
+            ))}
+          </Typography>
+          <Typography className={styles.position} variant="h4">
+            {'Software Developer'.split('').map((letter, i) => (
+              <span key={i}>{letter}</span>
+            ))}
+          </Typography>
+        </div>
       </div>
     </Layout>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,21 @@
-export function setIntesectionObserver(appendStyle: string, observeStyle: string): void {
+export function setIntesectionObserver(param: {
+  observeSelector: string;
+  appendStyle?: string;
+  callback?: (entry: IntersectionObserverEntry, index: number) => void;
+}): void {
+  const { observeSelector, appendStyle, callback } = param;
   const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add(appendStyle);
-      } else {
-        entry.target.classList.remove(appendStyle);
+    entries.forEach((entry, i) => {
+      if (callback) callback(entry, i);
+      else {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(appendStyle || '');
+        } else {
+          entry.target.classList.remove(appendStyle || '');
+        }
       }
     });
   });
-  const hiddenElements = document.querySelectorAll(`.${observeStyle}`);
+  const hiddenElements = document.querySelectorAll(observeSelector);
   hiddenElements.forEach((element) => observer.observe(element));
 }
